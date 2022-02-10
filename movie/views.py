7dc53f6a0410d14034d42326827886dd582abca5
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-02-10 11:04:56 -0500

    Cambios en la funcin home

--- movie/views.py
+++ movie/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return HttpResponse('<h1>Welcome Stiven to Home Page</h1>')
 
