642d6053327e2a40eaccfe512f173a02dcd1abd1
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-03-03 09:41:58 -0500

    formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    #return render(request, 'home.html', {'name':'Greg Lim'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
